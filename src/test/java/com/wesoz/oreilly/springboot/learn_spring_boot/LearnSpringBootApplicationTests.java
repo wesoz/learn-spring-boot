5c41e76ecb3d7c7d877623c2b0c147d505345a98
Wesley de Oliveira de Paula <32309310+wesoz@users.noreply.github.com>
2024-10-15 21:12:07 -0300

    Project setup

---
+++ src/test/java/com/wesoz/oreilly/springboot/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesoz.oreilly.springboot.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
